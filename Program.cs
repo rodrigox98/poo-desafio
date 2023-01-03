32be883d5212cde1c096190b639327d880961369
rodrigox98 <rodrigo.silva31@hotmail.com>
2023-01-03 18:30:24 -0300

    Completando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Nokia n1 = new Nokia("25", "NokiaGalaxy", "123456", 64);
+
+Iphone i1 = new Iphone("10", "Iphone6s", "78910", 32);
+
+n1.Ligar();
+i1.Ligar();
+
+n1.InstalarAplicativo("Angry birds");
+i1.InstalarAplicativo("Instagram");
+
+n1.ReceberLigacao();
+i1.ReceberLigacao();
